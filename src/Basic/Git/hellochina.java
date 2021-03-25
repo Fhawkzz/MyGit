a3fec6710cddcc7716d8269deb59c8e359ca79c2
Fhawk <768320322@qq.com>
2021-03-26 00:53:06 +0800

    third commit

--- src/Basic/Git/hellochina.java
+++ src/Basic/Git/hellochina.java
@@ -4,6 +4,8 @@ public class hellochina {
     public static void main(String[] args) {
         System.out.println("helloworld!");
         System.out.println("helloworld!");
+        System.out.println("helloworld!");
+        System.out.println("helloworld!");
 
     }
 }
