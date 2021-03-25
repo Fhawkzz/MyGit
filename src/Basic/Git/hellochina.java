e9fc4e70120963f0d9c1c82df9b79c6fb5344aba
Fhawk <768320322@qq.com>
2021-03-26 00:57:15 +0800

    Revert "third commit"
    This reverts commit a3fec671

---
+++ src/Basic/Git/hellochina.java
@@ -0,0 +1,11 @@
+package Basic.Git;
+
+public class hellochina {
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+        System.out.println("helloworld!");
+        System.out.println("helloworld!");
+        System.out.println("helloworld!");
+
+    }
+}
